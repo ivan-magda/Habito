13f8ab7560c63dd9913b83055f2c85ec8b2d63c2
bence0726 <benze726@gmail.com>
2017-05-04 23:24:43 +0200

    StringUtils test

---
+++ app/src/test/java/com/ivanmagda/habito/test/HabitoHabitListTest.java
@@ -0,0 +1,26 @@
+package com.ivanmagda.habito.test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import org.junit.runner.RunWith;
+import org.junit.*;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.ivanmagda.habito.models.Habit;
+import com.ivanmagda.habito.models.HabitList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Bence on 2017. 05. 04..
+ */
+
+@RunWith(MockitoJUnitRunner.class)
+public class HabitoHabitListTest {
+
+    
+}
